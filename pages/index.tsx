29e9c1de40323f621e52be68fe18032de60ae2eb
access <access@accessnoMacBook-Pro.local>
2021-01-11 20:02:39 +0900

    implement file upload function

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,56 +16,104 @@ export default function Home() {
 
 const Container = () => {
   // contextを状態として持つ
-  const [ context, setContext ] = useState(null);
+  const [ context, setContext ] = useState( null );
+
+  // フォントサイズを状態として持つ
+  const [ fontsize, setFontsize ] = useState( 55 );
 
   // コンポーネントの初期化完了後コンポーネント状態にコンテキストを登録
-  useEffect(() => {
-    const canvas: any = document.getElementById("canvas");
-
-    const canvasContext = canvas.getContext("2d");
-
-    setContext(canvasContext);
-  },[]);
+  useEffect( () => { initialize(); }, [] );
 
   // 状態にコンテキストが登録されたらそれに対して操作できる
-  useEffect(() => {
-    if( context !== null ) {
-      context.font = '80px ヒラギノ丸ゴ Pro W4';
+  // useEffect( () => { renderText(); }, [ context ] );
 
-      // 文字色を設定する
-      context.fillStyle = '#fff';
+  const initialize = () => {
+    const canvas: any = document.getElementById('canvas');
 
-      // アウトラインを設定する
-      context.strokeStyle = '#08003F';
-      context.lineWidth = 3;
+    const canvasContext = canvas.getContext('2d');
 
-      // ドロップシャドウを設定する
-      context.shadowColor = '#08003F';
-      context.shadowBlur = 0;
-      context.shadowOffsetX = 3;
-      context.shadowOffsetY = 1;
+    setContext(canvasContext);
+  }
 
-      // 表示オプションを設定する
-      context.textBaseline = 'middle';
+  const renderText = (text) => {
+    if( context === null ) return;
 
-      // テキストを描画する
-      context.strokeText('Hello World', 20, 100);
-      context.fillText('Hello World', 20, 100);
-    }
-  }, [ context ] );
+    // 文字オプションを設定する
+    context.font = `${fontsize}px ヒラギノ丸ゴ Pro W4`;
+    context.fillStyle = '#FFF';
+    context.textBaseline = 'middle';
+
+    // アウトラインを設定する
+    context.strokeStyle = '#08003F';
+    context.lineWidth = 3;
+
+    // ドロップシャドウを設定する
+    context.shadowColor = '#08003F';
+    context.shadowBlur = 0;
+    context.shadowOffsetX = 3;
+    context.shadowOffsetY = 1;
+
+    // テキストを描画する
+    context.strokeText(text, 20, 100);
+    context.fillText(text, 20, 100);
+  }
 
   const download = () => {
-    const canvas: any = document.getElementById("canvas");
-    const link = document.createElement("a");
-    link.href = canvas.toDataURL("image/png");
-    link.download = "test.png";
+    const canvas: any = document.getElementById('canvas');
+    
+    const link = document.createElement('a');
+    link.href = canvas.toDataURL('image/png');
+    link.download = 'test.png';
+    
     link.click();
+  }
+
+  const adjustPassage = ( passage: string ): object[] => {
+    const result: object[] = [];
+
+    const regExp = new RegExp('[N|A|B|C|E]');
+
+    const text = passage.replace(/\r?\n/g, '');
+    
+    let s = 0;
+
+    for(let i = 0; i < text.length; i++) {
+      if( regExp.test( text.charAt( i ) ) === false ) continue
+
+      if(s >= i) continue;
+
+      result.push( { [ text.slice( s, s + 1 ) ] : text.slice( s, i ) } );
+
+      s = i;
+    }
+
+    return result;
+  }
+
+  const onChange = (e) => {
+    const files = e.target.files;
+
+    if( files.length < 1 ) return;
+
+    const reader = new FileReader();
+    
+    reader.readAsText(files[0]);
+    
+    reader.onload = () => {
+      const text: string = reader.result.toString();
+
+      const test: any = adjustPassage(text);
+
+      console.log(test)
+    }
   }
 
   return (
     <div>
       <canvas id="canvas" width="1280" height="720"></canvas>
+      <input type="file" onChange={onChange} />
       <Button className="mybutton" value="download" onClick={ download } />
+      <pre id="pre1"></pre>
     </div>
   );
 }
