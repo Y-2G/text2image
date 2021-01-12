79b65f472793c6da37be60430508427272f9c682
Yoshifumi-T <2g.4423@gmail.com>
2021-01-12 23:09:30 +0900

    implement the function to change dynamic text settings

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,12 +14,23 @@ export default function Home() {
   )
 }
 
+const MIN_X = 0;
+const MAX_X = 500;
+
+const TEXT_MARGIN = 1.2;
+
+type settings = {
+  font: string,
+  color: string,
+  outline: string,
+  size: number,
+  height: number,
+  align: string
+}
+
 const Container = () => {
   // contextを状態として持つ
   const [ context, setContext ] = useState( null );
-
-  // フォントサイズを状態として持つ
-  const [ fontsize, setFontsize ] = useState( 55 );
 
   // コンポーネントの初期化完了後コンポーネント状態にコンテキストを登録
   useEffect( () => { initialize(); }, [] );
@@ -35,37 +46,101 @@ const Container = () => {
     setContext(canvasContext);
   }
 
-  const renderText = (text) => {
-    if( context === null ) return;
+  const divideTextSettings = (type) : settings => {
+    const result: settings = {
+      font: '',
+      color: '',
+      outline: '',
+      size: 0,
+      height: 0,
+      align: ''
+    };
+
+    switch ( type ) {
+      case 'N':
+        result.font  = 'ヒラギノ丸ゴ Pro W4';
+        result.color = '#FFF';
+        result.outline = '#000';
+        result.size  = 30;
+        result.height = 1.1618;
+        result.align = 'left';
+        break;
+      case 'A':
+        result.font  = 'ヒラギノ丸ゴ Pro W4';
+        result.color = '#FFF';
+        result.outline = '#08003F';
+        result.size  = 50;
+        result.height = 1;
+        result.align = 'left';
+        break;
+      case 'B':
+        result.font  = 'ヒラギノ丸ゴ Pro W4';
+        result.color = '#FFF';
+        result.outline = 'red';
+        result.size  = 50;
+        result.height = 1;
+        result.align = 'left';
+        break;
+      case 'C':
+        result.font  = 'ヒラギノ丸ゴ Pro W4';
+        result.color = '#FFF';
+        result.outline = 'green';
+        result.size  = 50;
+        result.height = 1;
+        result.align = 'left';
+        break;
+    }
+
+    return result;
+  }
+
+  const renderText = ( type, text ) => {
+    if( context === null || text === '' ) return;
+
+    console.log(text)
+
+    const canvas: any = document.getElementById('canvas');
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    const settings = divideTextSettings(type);
 
     // 文字オプションを設定する
-    context.font = `${fontsize}px ヒラギノ丸ゴ Pro W4`;
-    context.fillStyle = '#FFF';
-    context.textBaseline = 'middle';
+    context.font = `${settings.size}px ${settings.font}`;
+    context.fillStyle = settings.color;
+    context.textAlign = settings.align;
+    context.textBaseline = 'top';
 
     // アウトラインを設定する
-    context.strokeStyle = '#08003F';
+    context.strokeStyle = settings.outline;
     context.lineWidth = 3;
 
     // ドロップシャドウを設定する
-    context.shadowColor = '#08003F';
+    context.shadowColor = settings.outline;
     context.shadowBlur = 0;
     context.shadowOffsetX = 3;
     context.shadowOffsetY = 1;
 
-    // テキストを描画する
-    context.strokeText(text, 20, 100);
-    context.fillText(text, 20, 100);
-  }
+    let str = '';
 
-  const download = () => {
-    const canvas: any = document.getElementById('canvas');
+    let y = 0;
+    let x = 0;
     
-    const link = document.createElement('a');
-    link.href = canvas.toDataURL('image/png');
-    link.download = 'test.png';
-    
-    link.click();
+    for( let i = 0; i < text.length; i++ ) {
+      const mesure = context.measureText( str + text.charAt( i ) );
+      if( mesure.width > MAX_X ) {
+        str = '';
+        y += settings.size * settings.height;
+      }
+
+      str += text.charAt( i );
+
+      context.strokeText( str, x, y );
+      context.fillText( str, x, y );
+    }
+   
+    y += settings.size * ( settings.height * TEXT_MARGIN );
+  
+    download();
   }
 
   const adjustPassage = ( passage: string ): object[] => {
@@ -73,16 +148,14 @@ const Container = () => {
 
     const regExp = new RegExp('[N|A|B|C|E]');
 
-    const text = passage.replace(/\r?\n/g, '');
-    
     let s = 0;
 
-    for(let i = 0; i < text.length; i++) {
-      if( regExp.test( text.charAt( i ) ) === false ) continue
+    for(let i = 0; i < passage.length; i++) {
+      if( regExp.test( passage.charAt( i ) ) === false ) continue
 
       if(s >= i) continue;
 
-      result.push( { [ text.slice( s, s + 1 ) ] : text.slice( s, i ) } );
+      result.push( { [ passage.slice( s, s + 1 ) ] : passage.slice( s + 2, i ).split('\n') } );
 
       s = i;
     }
@@ -100,12 +173,33 @@ const Container = () => {
     reader.readAsText(files[0]);
     
     reader.onload = () => {
-      const text: string = reader.result.toString();
+      const passage: string = reader.result.toString();
 
-      const test: any = adjustPassage(text);
+      const result: any = adjustPassage(passage);
 
-      console.log(test)
+      test(result)
     }
+  }
+
+  const test = (passage) => {
+    if( passage === null ) return;
+
+    for( const e of passage ) {
+      for( const k of Object.keys( e ) ) {
+        const arr: [] = e[k];
+        arr.forEach( e => renderText(k, e) );
+      }
+    }
+  }
+
+  const download = () => {
+    const canvas: any = document.getElementById('canvas');
+    
+    const link = document.createElement('a');
+    link.href = canvas.toDataURL('image/png');
+    link.download = 'test.png';
+    
+    link.click();
   }
 
   return (
@@ -113,7 +207,6 @@ const Container = () => {
       <canvas id="canvas" width="1280" height="720"></canvas>
       <input type="file" onChange={onChange} />
       <Button className="mybutton" value="download" onClick={ download } />
-      <pre id="pre1"></pre>
     </div>
   );
 }
