a976e9fd4893a272ff688d8ec7005d5e76cbab99
access <access@accessnoMacBook-Pro.local>
2021-01-27 14:35:49 +0900

    change setting

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
+require('dotenv').config();
+
 module.exports = {
-    // some configuration
-    assetPrefix: process.env.NODE_ENV === "production" ? "/project_name" : ""
-    // another configuration
+  assetPrefix: process.env.GITHUB_PAGES ? '/repository-name' : '',
 };
-  
